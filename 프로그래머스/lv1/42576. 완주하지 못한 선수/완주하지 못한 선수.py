f8d30f62d23def59c43da123f20acafff9631fa5
nanyoojinee <126642229+nanyoojinee@users.noreply.github.com>
2023-05-11 20:05:16 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 69.92 ms, Memory: 26.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -21,15 +21,11 @@
 #     return list(answer)[0]
 
 def solution(participant, completion):
-    # 참가자 리스트를 정렬
     participant.sort()
-    # 완주자 리스트를 정렬
     completion.sort()
-    
-    # 두 리스트를 비교하여 완주하지 못한 선수 찾기
+   
     for i in range(len(completion)):
         if participant[i] != completion[i]:
             return participant[i]
     
-    # 모든 완주자를 제외한 마지막 선수가 완주하지 못한 선수임
     return participant[-1]
